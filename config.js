1c7960aad1c7649d22354dee7486f8e1dc9154e6
anhtvgch190350 <77433425+anhtvgch190350@users.noreply.github.com>
2021-09-04 02:06:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Hương à!',
+    introTitle: 'Mia à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là Hương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    Đằng sau chữ yêu đây là Thương`,
+    btnIntro: 'Chúc mừng sinh nhật công chúa',
+    title: 'Cảm ơn ông trời đã mang thiên thần 16 tuổi đến bên anh ',
+    desc: 'Tặng em ngàn lời yêu và những điều tuyệt vời nhất ',
+    btnYes: 'Em yêu anh <3',
+    btnNo: 'Em cảm ơn anh trai :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Công chúa bấm vào nút dưới để thấy điều bất ngờ',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.youtube.com/watch?v=taNXwSP7Zqs&list=RDtaNXwSP7Zqs&start_radio=1' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
